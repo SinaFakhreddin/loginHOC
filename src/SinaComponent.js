a49456ea27402524ba51ba973428178b52f6f962
Sina Fakhreddin <sina.fakhreddin@gmail.com>
2023-06-27 15:42:20 +0330

    first commit

---
+++ src/SinaComponent.js
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+
+const SinaComponent = () => {
+  return (
+    <div>
+      <h2>Sina Has $20</h2>
+      <button>increase Sina's Money</button>
+    </div>
+  );
+};
+
+export default SinaComponent;
